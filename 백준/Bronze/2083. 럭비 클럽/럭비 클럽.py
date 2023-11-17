bd2448d77bbc7f23e7a4f570d6663cb25613437b
yewon <98067634+developeryw@users.noreply.github.com>
2023-11-17 21:27:30 +0900

    [Bronze IV] Title: 럭비 클럽, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2083. 럭비 클럽/럭비 클럽.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+result = []
+
+while True:
+    name, age, weight = input().split()
+    age = int(age)
+    weight = int(weight)
+
+    if name == '#' and age == 0 and weight == 0:
+        break
+
+    if age > 17 or weight >= 80:
+        result.append(name + " Senior")
+    else:
+        result.append(name + " Junior")
+
+for r in result:
+    for k in r:
+        print(k, end="")
+    print()
