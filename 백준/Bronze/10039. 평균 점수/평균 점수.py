44d398d96c52e076919785fc45f4934df3710b97
yewon <98067634+developeryw@users.noreply.github.com>
2023-11-24 02:28:41 +0900

    [Bronze IV] Title: 평균 점수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10039. 평균 점수/평균 점수.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+sum = 0
+
+for i in range(5):
+    score = int(input())
+
+    if score < 40:
+        score = 40
+
+    sum += score
+
+print(int(sum / 5))
