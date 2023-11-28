06ba20e924e0d5edf5cc7c0456a562177d0dc764
yewon <98067634+developeryw@users.noreply.github.com>
2023-11-29 00:05:28 +0900

    [Silver V] Title: BABBA, Time: 40 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+K = int(input())
+A = [0] * (K+1)
+B = [0] * (K+1)
+
+A[0] = 1
+B[0] = 0
+
+for i in range(1, K+1):
+    A[i] = B[i-1]
+    B[i] = A[i-1] + B[i-1]
+
+print(A[K], B[K])
