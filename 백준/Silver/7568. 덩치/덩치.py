45d4411cf9f7f18c8a140896421fb1b82ea74cb6
yewon <98067634+developeryw@users.noreply.github.com>
2024-01-01 23:14:25 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+people = []
+
+for i in range(N):
+    w, h = map(int, input().split())
+    temp = {'weight': w, 'height': h, 'index': i}
+    people.append(temp)
+
+rankings = []
+
+for j in range(N):
+    rank = 1
+
+    for k in range(N):
+        if people[k]['weight'] > people[j]['weight'] and people[j]['height'] < people[k]['height']:
+            if not 'rank' in people[k].keys():
+                rank += 1
+
+    rankings.append(rank)
+
+for r in rankings:
+    print(r, end=" ")
