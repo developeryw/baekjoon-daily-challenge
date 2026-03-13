cc781d81d44223a4a8ab1ea7757e4ad5577b6221
차예원 Yewon Cha <98067634+developeryw@users.noreply.github.com>
2026-03-13 16:11:08 +0900

    [Silver IV] Title: 주몽, Time: 396 ms, Memory: 21852 KB -BaekjoonHub

---
+++ Java/백준/Silver/1940. 주몽/주몽.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+  public static void main(String args[]) throws IOException {
+    BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
+    StringTokenizer st;
+
+    st = new StringTokenizer(br.readLine());
+    int n = Integer.parseInt(st.nextToken());
+
+    st = new StringTokenizer(br.readLine());
+    int m = Integer.parseInt(st.nextToken());
+
+    int[] arr = new int[n];
+
+    int result = 0;
+
+    st = new StringTokenizer(br.readLine());
+    for (int i = 0; i < n; i++) {
+      arr[i] = Integer.parseInt(st.nextToken());
+    }
+
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (arr[i] + arr[j] == m) {
+          result += 1;
+        }
+      }
+    }
+
+    System.out.println(result);
+  }
+}
